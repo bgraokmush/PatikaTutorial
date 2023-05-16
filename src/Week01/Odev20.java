293f32931441a231f4ca678184480a9002a5c148
bugra <saltuk.bugraokumus@gmail.com>
2023-05-16 16:15:00 +0300

    Odev20 updated

--- src/Week01/Odev20.java
+++ src/Week01/Odev20.java
@@ -11,12 +11,23 @@ public class Odev20 {
         System.out.println("Sayi giriniz: ");
         int sayi = scanner.nextInt();
         System.out.println("Sonuç->" + sayi + " sayısının\n4'ün kuvvetleri: " + kuvvet(sayi, 4) + "\n5'in Kuvvetleri: " + kuvvet(sayi, 5));
+
+        System.out.println("\nFactorial hesaplama: " + faktoriyel(sayi));
     }
 
     public static String kuvvet(int sayi, int kat){
         String sonuc = "";
         for (int i = 0; i <= sayi; i+=kat) {
             sonuc += i + ", ";
+        }
+        return sonuc;
+    }
+
+    //Faktöriyel hesaplama
+    public static int faktoriyel(int sayi){
+        int sonuc = 1;
+        for (int i = 1; i <= sayi; i++) {
+            sonuc *= i;
         }
         return sonuc;
     }

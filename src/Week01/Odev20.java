9a0be4f5b653311a8a46da9630225053dce75ac0
bugra <saltuk.bugraokumus@gmail.com>
2023-05-16 16:12:58 +0300

    Odev20 added

---
+++ src/Week01/Odev20.java
@@ -0,0 +1,23 @@
+package Week01;
+
+import java.util.Scanner;
+
+/*
+    Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz.
+*/
+public class Odev20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sayi giriniz: ");
+        int sayi = scanner.nextInt();
+        System.out.println("Sonuç->" + sayi + " sayısının\n4'ün kuvvetleri: " + kuvvet(sayi, 4) + "\n5'in Kuvvetleri: " + kuvvet(sayi, 5));
+    }
+
+    public static String kuvvet(int sayi, int kat){
+        String sonuc = "";
+        for (int i = 0; i <= sayi; i+=kat) {
+            sonuc += i + ", ";
+        }
+        return sonuc;
+    }
+}

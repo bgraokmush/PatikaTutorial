91f15946cefe71e8f4a2f4dccde3a67dfbf6fddb
bugra <saltuk.bugraokumus@gmail.com>
2023-05-16 15:58:18 +0300

    Odev16 and Odev17 added

---
+++ src/Week01/Odev17.java
@@ -0,0 +1,24 @@
+package Week01;
+
+import java.util.Scanner;
+
+/*
+    Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+*/
+public class Odev17 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Sayi giriniz: ");
+        int sayi = s.nextInt();
+        int toplam = 0;
+
+        for (int i = 0; i <= sayi; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                toplam += i;
+            }
+        }
+        System.out.println("Toplam -> " + toplam);
+    }
+
+}

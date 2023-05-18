d8a325eb64e2a6c83891f108e910232f647c3eb0
bugra <saltuk.bugraokumus@gmail.com>
2023-05-18 15:43:54 +0300

    Odev30 added

---
+++ src/Week01/Odev30.java
@@ -0,0 +1,25 @@
+package Week01;
+
+import java.util.Scanner;
+
+/*
+    Java döngüler ile fibonacci serisi bulan program yazıyoruz.
+    Fibonacci serisinin eleman sayısını kullanıcıdan alın.
+*/
+public class Odev30 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Fibonacci serisinin eleman sayisini giriniz:");
+        int n = scanner.nextInt();
+        int a = 0;
+        int b = 1;
+        int c;
+        System.out.print(a + ", " + b);
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            System.out.print(", " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
